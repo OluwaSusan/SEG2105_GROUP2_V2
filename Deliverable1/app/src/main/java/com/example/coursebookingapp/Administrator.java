31a60f4d56928b7b008a113131240c34e78662dc
Poompoomi <proulxlive@gmail.com>
2021-06-15 14:11:05 -0400

    Simon: admin btns v1

--- Deliverable1/app/src/main/java/com/example/coursebookingapp/Administrator.java
+++ Deliverable1/app/src/main/java/com/example/coursebookingapp/Administrator.java
@@ -1,6 +1,8 @@
 package com.example.coursebookingapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,11 +11,34 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class Administrator extends AppCompatActivity {
 
+    Button viewusers_admin, homeBtn_admin;
+    com.google.android.material.floatingactionbutton.FloatingActionButton coursebtn_add;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_administrator);
 
+        viewusers_admin = findViewById(R.id.viewusers_admin);
+
+        viewusers_admin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        homeBtn_admin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        coursebtn_add.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
-
-
 }
