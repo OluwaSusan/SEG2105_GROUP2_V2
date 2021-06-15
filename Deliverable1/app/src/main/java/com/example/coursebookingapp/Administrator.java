4e7ce9351d9821888babe037401ddb3ce8aa64e4
Poompoomi <proulxlive@gmail.com>
2021-06-15 14:41:58 -0400

    Simon: basic admin_btn functionality done, missing student/course (dbhandler) functionality

--- Deliverable1/app/src/main/java/com/example/coursebookingapp/Administrator.java
+++ Deliverable1/app/src/main/java/com/example/coursebookingapp/Administrator.java
@@ -1,5 +1,6 @@
 package com.example.coursebookingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,7 +12,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class Administrator extends AppCompatActivity {
 
-    Button viewusers_admin, homeBtn_admin;
+    Button viewusers_admin, homeBtn_admin, backBtn_admin;
     com.google.android.material.floatingactionbutton.FloatingActionButton coursebtn_add;
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,18 +20,22 @@ public class Administrator extends AppCompatActivity {
         setContentView(R.layout.activity_administrator);
 
         viewusers_admin = findViewById(R.id.viewusers_admin);
+        homeBtn_admin = findViewById(R.id.homeBtn_admin);
+        backBtn_admin = findViewById(R.id.backBtn_admin);
+        coursebtn_add = findViewById(R.id.coursebtn_add);
+
 
         viewusers_admin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                setContentView(R.layout.activity_userlist);
             }
         });
         homeBtn_admin.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-
+                setContentView(R.layout.activity_welcome);
             }
         });
         coursebtn_add.setOnClickListener(new View.OnClickListener() {
@@ -38,6 +43,13 @@ public class Administrator extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+            }
+        });
+        backBtn_admin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_administrator);
             }
         });
     }
