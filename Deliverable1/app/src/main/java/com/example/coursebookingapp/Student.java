b363625eadf8d6903bc00476109bb968e30ad652
NaomiH167 <44326916+NaomiH167@users.noreply.github.com>
2021-06-15 19:57:30 -0400

    student.java

---
+++ Deliverable1/app/src/main/java/com/example/coursebookingapp/Student.java
@@ -0,0 +1,24 @@
+package com.example.coursebookingapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class Student extends AppCompatActivity {
+
+
+    public Student( String userName, String password, String FullName){
+
+
+    }
+
+
+}
