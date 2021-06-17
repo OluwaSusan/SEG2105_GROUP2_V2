73a814c8f3668f498e19af9cae2f061b825c9385
Poompoomi <proulxlive@gmail.com>
2021-06-17 13:03:44 -0400

    Simon: CourseActivity functionality v0.5

--- Deliverable1/app/src/main/java/com/example/coursebookingapp/CourseActivity.java
+++ Deliverable1/app/src/main/java/com/example/coursebookingapp/CourseActivity.java
@@ -1,19 +1,55 @@
 package com.example.coursebookingapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class CourseActivity extends Activity {
+
+    Button save_btn;
+    TextInputLayout course_name, course_id, course_description;
+    TextView errorCourseEnter;
+    DBHandlerCourses dbCourses;
+    Course foundCourse;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course);
+        //putExtra()
+        save_btn = findViewById(R.id.save_btn);
+        dbCourses = new DBHandlerCourses();
 
 
+        save_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkValidation();
+                saveCourse();
+                closeWindow();
+            }
+        });
+
+    }
+
+    private void closeWindow() {
+    }
+
+    private void saveCourse() {
+
+    }
+
+    private void checkValidation() {
     }
 
 }
