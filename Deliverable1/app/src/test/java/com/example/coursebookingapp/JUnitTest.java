6b9fa4f144efcf767ad5bee598a71846480ccc16
Poompoomi <proulxlive@gmail.com>
2021-06-30 15:53:30 -0400

    Simon/Naomi: fixed JUnit bug

--- Deliverable1/app/src/test/java/com/example/coursebookingapp/JUnitTest.java
+++ Deliverable1/app/src/test/java/com/example/coursebookingapp/JUnitTest.java
@@ -1,25 +1,8 @@
 package com.example.coursebookingapp;
 
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.when;
 
 import static org.junit.Assert.*;
 import com.example.coursebookingapp.MainActivity;
@@ -27,23 +10,16 @@ import com.example.coursebookingapp.CourseActivity;
 
 
 public class JUnitTest {
-    Private DatabaseReference mockedDatabaseReference;
+    private MainActivity main;
 
     @Before
-    public void before() {
-        mockedDatabaseReference = Mockito.mock(DatabaseReference.class);
-
-        FirebaseDatabase mockedFirebaseDatabase = Mockito.mock(FirebaseDatabase.class);
-        when(mockedFirebaseDatabase.getReference()).thenReturn(mockedDatabaseReference);
-
-        PowerMockito.mockStatic(FirebaseDatabase.class);
-        when(FirebaseDatabase.getInstance()).thenReturn(mockedFirebaseDatabase);
+    public void setUp() {
+        main = new MainActivity();
     }
 
     @Test
     public void validation_full_name(){
-        MainActivity main = new MainActivity();
-        boolean result = main.validFullName("Stevieboy123");
+        boolean result = main.validFullName("Steve McQueen");
         assertTrue(result);
     }
 
