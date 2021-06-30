8e340636e38057f6ff90591779e6efe4ef193c91
NaomiH167 <44326916+NaomiH167@users.noreply.github.com>
2021-06-30 15:17:28 -0400

    mock firebase implementation

--- Deliverable1/app/src/test/java/com/example/coursebookingapp/JUnitTest.java
+++ Deliverable1/app/src/test/java/com/example/coursebookingapp/JUnitTest.java
@@ -1,6 +1,25 @@
 package com.example.coursebookingapp;
 
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.when;
 
 import static org.junit.Assert.*;
 import com.example.coursebookingapp.MainActivity;
@@ -8,21 +27,18 @@ import com.example.coursebookingapp.CourseActivity;
 
 
 public class JUnitTest {
+    Private DatabaseReference mockedDatabaseReference;
 
-//    CourseActivity course = new CourseActivity();
+    @Before
+    public void before() {
+        mockedDatabaseReference = Mockito.mock(DatabaseReference.class);
 
+        FirebaseDatabase mockedFirebaseDatabase = Mockito.mock(FirebaseDatabase.class);
+        when(mockedFirebaseDatabase.getReference()).thenReturn(mockedDatabaseReference);
 
-    String str;
-
-
-
-
-//    @Test
-//    public void validation_courses(){
-//
-//        str = course.checkValidation("Introduction to Discrete Math", "MAT1348");
-//        assertThat(str,null);
-//    }
+        PowerMockito.mockStatic(FirebaseDatabase.class);
+        when(FirebaseDatabase.getInstance()).thenReturn(mockedFirebaseDatabase);
+    }
 
     @Test
     public void validation_full_name(){
@@ -36,5 +52,11 @@ public class JUnitTest {
 //        assertThat(main.userNameExists.isTrue());
 //    }
 
+//    @Test
+//    public void validation_courses(){
+//
+//        str = course.checkValidation("Introduction to Discrete Math", "MAT1348");
+//        assertThat(str,null);
+//    }
 
 }
